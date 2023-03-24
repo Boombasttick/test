cc3fa95de8d3f0c3f61abf429bb388fa2d0f1ff4
Boombasttick <17865566719.163.com>
2023-03-24 09:21:17 +0800

    250230324

---
+++ hallo.c
@@ -0,0 +1,42 @@
+// #include <stdio.h>
+// //#include <string.h>
+
+// //#define max 10
+// void test()
+// {
+//     static int a=1;
+//     a++;
+//     printf("a = %d\n",a);
+// }
+// int main(){
+//     //int a = 5 ;
+//     //char arr[10] = {0};
+//     //int a = 2;
+//     //scanf ("%d",&arr);
+//     //printf ("%d\n",strlen("c:\test\32\test.c"));
+//     //printf("c:\test\32\test.c");
+//     //printf("%c\n",'\60');
+  
+//     // int ar[5] = {1,2,3,4,5};
+//     // int i = 0;
+//     // while (i<4)
+//     // {
+//     // printf ("%d \n",ar[i]);
+//     // i++;
+//     // }
+//     //int a=10;
+//     //int b=a--;
+    
+
+//     //b=a&3;
+//     //printf("%d\n%d\n%d",a,b,c);
+//     int i=0;
+//     while (i<5){
+//         test ();
+//         i++;
+//     }
+
+
+//     return 0;
+    
+// }
